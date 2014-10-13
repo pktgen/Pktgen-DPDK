94dc2b5d0ea39609c68cc0ecb389d699b6596925
Keith Wiles <keith.wiles@windriver.com>
2014-10-13 15:14:42 -0500

    Add new test lua file and link to lua
    Signed-off-by: Keith Wiles <keith.wiles@windriver.com>

---
+++ test/info.lua
@@ -0,0 +1,17 @@
+package.path = package.path ..";?.lua;test/?.lua;app/?.lua;"
+
+-- A list of the test script for Pktgen and Lua.
+-- Each command somewhat mirrors the pktgen command line versions.
+-- A couple of the arguments have be changed to be more like the others.
+--
+pktgen.screen("off");
+pktgen.pause("Screen off\n", 2000);
+
+printf("Lua Vesrion      : %s\n", pktgen.info.Lua_Version);
+printf("Pktgen Version   : %s\n", pktgen.info.Pktgen_Version);
+printf("Pktgen Copyright : %s\n", pktgen.info.Pktgen_Copyright);
+
+prints("pktgen.info", pktgen.info);
+
+printf("Port Count %d\n", pktgen.portCount());
+printf("Total port Count %d\n", pktgen.totalPorts());

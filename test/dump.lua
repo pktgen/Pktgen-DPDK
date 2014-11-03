43975035fcf0caa9cf9a6cf7d60c0f12942c49d6
Keith Wiles <keith.wiles@windriver.com>
2014-11-02 18:49:20 -0600

    Fix typo in message output
    Signed-off-by: Keith Wiles <keith.wiles@windriver.com>

--- test/dump.lua
+++ test/dump.lua
@@ -2,7 +2,7 @@ package.path = package.path ..";?.lua;test/?.lua;app/?.lua;"
 
 pktgen.screen("off");
 
-printf("Lua Vesrion      : %s\n", pktgen.info.Lua_Version);
+printf("Lua Version      : %s\n", pktgen.info.Lua_Version);
 printf("Pktgen Version   : %s\n", pktgen.info.Pktgen_Version);
 printf("Pktgen Copyright : %s\n", pktgen.info.Pktgen_Copyright);
 printf("Pktgen Authors   : %s\n", pktgen.info.Pktgen_Authors);
--- test/info.lua
+++ test/info.lua
@@ -7,7 +7,7 @@ package.path = package.path ..";?.lua;test/?.lua;app/?.lua;"
 pktgen.screen("off");
 pktgen.pause("Screen off\n", 2000);
 
-printf("Lua Vesrion      : %s\n", pktgen.info.Lua_Version);
+printf("Lua Version      : %s\n", pktgen.info.Lua_Version);
 printf("Pktgen Version   : %s\n", pktgen.info.Pktgen_Version);
 printf("Pktgen Copyright : %s\n", pktgen.info.Pktgen_Copyright);
 

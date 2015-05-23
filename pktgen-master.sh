8267b620535b92832785dfc29468e42dfc40f9f8
Keith Wiles <keith.wiles@intel.com>
2015-05-23 18:43:54 -0500

    Add files to run two instances
    Signed-off-by: Keith Wiles <keith.wiles@intel.com>

---
+++ pktgen-master.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Normal setup
+#   different cores for each port.
+
+name=`uname -n`
+
+# Use 'sudo -E ./setup.sh' to include environment variables
+
+if [ -z ${RTE_SDK} ] ; then
+    echo "*** RTE_SDK is not set, did you forget to do 'sudo -E ./setup.sh'"
+	export RTE_SDK=/work/home/rkwiles/projects/intel/dpdk
+	export RTE_TARGET=x86_64-native-linuxapp-clang
+fi
+sdk=${RTE_SDK}
+
+if [ -z ${RTE_TARGET} ]; then
+    echo "*** RTE_TARGET is not set, did you forget to do 'sudo -E ./setup.sh'"
+    target=x86_64-pktgen-linuxapp-gcc
+else
+    target=${RTE_TARGET}
+fi
+
+
+#rkwiles@rkwiles-desk:~/projects/intel/dpdk$ lspci |grep Ether
+#06:00.0 Ethernet controller: Intel Corporation Ethernet Converged Network Adapter X520-Q1 (rev 01)
+#06:00.1 Ethernet controller: Intel Corporation Ethernet Converged Network Adapter X520-Q1 (rev 01)
+#08:00.0 Ethernet controller: Intel Corporation Ethernet Converged Network Adapter X520-Q1 (rev 01)
+#08:00.1 Ethernet controller: Intel Corporation Ethernet Converged Network Adapter X520-Q1 (rev 01)
+#09:00.0 Ethernet controller: Intel Corporation I350 Gigabit Network Connection (rev 01)
+#09:00.1 Ethernet controller: Intel Corporation I350 Gigabit Network Connection (rev 01)
+#83:00.1 Ethernet controller: Intel Corporation DH8900CC Null Device (rev 21)
+#87:00.0 Ethernet controller: Intel Corporation Ethernet Converged Network Adapter X520-Q1 (rev 01)
+#87:00.1 Ethernet controller: Intel Corporation Ethernet Converged Network Adapter X520-Q1 (rev 01)
+#89:00.0 Ethernet controller: Intel Corporation Ethernet Converged Network Adapter X520-Q1 (rev 01)
+#89:00.1 Ethernet controller: Intel Corporation Ethernet Converged Network Adapter X520-Q1 (rev 01)
+
+if [ $name == "rkwiles-supermicro" ]; then
+./app/app/${target}/pktgen -c ff2 -n 3 --proc-type auto --socket-mem 512,512 --file-prefix pg1 -b 09:00.0 -b 09:00.1 -b 83:00.1 -b 06:00.0 -b 06:00.1 -b 08:00.0 -b 08:00.1 -- -T -P -m "[4:6].0, [5:7].1, [8:10].2, [9:11].3" -f themes/black-yellow.theme
+fi

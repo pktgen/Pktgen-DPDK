4eef50f0b9bd25beff435d8ef06a361e9069bba5
Keith Wiles <keith.wiles@intel.com>
2016-02-24 09:35:35 -0600

    Add new rf2544 lua script
    Signed-off-by: Keith Wiles <keith.wiles@intel.com>

---
+++ scripts/rfc2544_tput_test.lua
@@ -0,0 +1,106 @@
+-- RFC2544 Throughput Test
+-- as defined by https://www.ietf.org/rfc/rfc2544.txt
+
+package.path = package.path ..";?.lua;test/?.lua;app/?.lua;../?.lua"
+
+require "Pktgen";
+
+-- define packet sizes to test
+local pkt_sizes		= { 64, 128, 256, 512, 1024, 1280, 1518 };
+
+-- Time in seconds to transmit for
+local duration		= 10000;
+local confirmDuration	= 60000;
+local pauseTime		= 1000;
+
+-- define the ports in use
+local sendport		= "0";
+local recvport		= "1";
+
+-- ip addresses to use
+local dstip		= "90.90.90.90";
+local srcip		= "1.1.1.1";
+local netmask		= "/24";
+
+local initialRate	= 50 ;
+
+local function setupTraffic()
+	pktgen.set_ipaddr(sendport, "dst", dstip);
+	pktgen.set_ipaddr(sendport, "src", srcip..netmask);
+
+	pktgen.set_ipaddr(recvport, "dst", srcip);
+	pktgen.set_ipaddr(recvport, "src", dstip..netmask);
+
+	pktgen.set_proto(sendport..","..recvport, "udp");
+	-- set Pktgen to send continuous stream of traffic
+	pktgen.set(sendport, "count", 0);
+end
+
+local function runTrial(pkt_size, rate, duration, count)
+	local num_tx, num_rx, num_dropped;
+
+	pktgen.clr();
+	pktgen.set(sendport, "rate", rate);
+
+	pktgen.start(sendport);
+	print("Running trial " .. count .. ". % Rate: " .. rate .. ". Packet Size: " .. pkt_size .. ". Duration (mS):" .. duration);
+	file:write("Running trial " .. count .. ". % Rate: " .. rate .. ". Packet Size: " .. pkt_size .. ". Duration (mS):" .. duration .. "\n");
+	pktgen.delay(duration);
+	pktgen.stop(sendport);
+
+	pktgen.delay(pauseTime);
+
+	statTx = pktgen.portStats(sendport, "port")[tonumber(sendport)];
+	statRx = pktgen.portStats(recvport, "port")[tonumber(recvport)];
+	num_tx = statTx.opackets;
+	num_rx = statRx.ipackets;
+	num_dropped = num_tx - num_rx;
+
+	print("Tx: " .. num_tx .. ". Rx: " .. num_rx .. ". Dropped: " .. num_dropped);
+	file:write("Tx: " .. num_tx .. ". Rx: " .. num_rx .. ". Dropped: " .. num_dropped .. "\n");
+	pktgen.delay(pauseTime);
+
+	return num_dropped;
+end
+
+local function runThroughputTest(pkt_size)
+	local num_dropped, max_rate, min_rate, trial_rate;
+
+	max_rate = 100;
+	min_rate = 1;
+	trial_rate = initialRate;
+	for count=1, 10, 1
+	do		
+		num_dropped = runTrial(pkt_size, trial_rate, duration, count);
+		if num_dropped == 0
+		then
+			min_rate = trial_rate;
+		else
+			max_rate = trial_rate;
+		end
+		trial_rate = min_rate + ((max_rate - min_rate)/2);
+	end
+
+	-- confirm throughput rate for at least 60 seconds
+	num_dropped = runTrial(pkt_size, trial_rate, confirmDuration, "Confirmation");
+	if num_dropped == 0
+	then
+		print("Max rate for packet size "  .. pkt_size .. "B is: " .. trial_rate);
+		file:write("Max rate for packet size "  .. pkt_size .. "B is: " .. trial_rate .. "\n\n");
+	else
+		print("Max rate of " .. trial_rate .. "% could not be confirmed for 60 seconds as required by rfc2544.");
+		file:write("Max rate of " .. trial_rate .. "% could not be confirmed for 60 seconds as required by rfc2544." .. "\n\n");
+	end
+end
+
+function main()
+	file = io.open("RFC2544_throughput_results.txt", "w");
+	setupTraffic();
+	for _,size in pairs(pkt_sizes)
+	do
+		runThroughputTest(size);
+	end
+	file:close();
+end
+
+main();

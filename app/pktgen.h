8a4d8fcf100ca262ec189b8082ac8011336f6c18
Keith Wiles <keith.wiles@intel.com>
2017-08-15 14:23:47 -0500

    update version to 3.4.0
    Signed-off-by: Keith Wiles <keith.wiles@intel.com>

--- app/pktgen.h
+++ app/pktgen.h
@@ -121,7 +121,7 @@
 extern "C" {
 #endif
 
-#define PKTGEN_VERSION          "3.3.9"
+#define PKTGEN_VERSION          "3.4.0"
 #define PKTGEN_APP_NAME         "Pktgen"
 #define PKTGEN_CREATED_BY       "Keith Wiles"
 

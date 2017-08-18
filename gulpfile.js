8e770a900e0408ee669cb434e1253ce85cd925b9
Keith Wiles <keith.wiles@intel.com>
2017-08-18 18:17:10 -0500

    update for vscode files we need to keep
    Signed-off-by: Keith Wiles <keith.wiles@intel.com>

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,16 @@
 var cp = require('child_process');
 var gulp = require('gulp');
-gulp.task('build', function () {
-    var env = process.env.RTE_TARGET || 'x86_64-native-linuxapp-gcc';
+gulp.task('default', function () {
+    var sdk = global.process.env.RTE_SDK;
+    var target = process.env.RTE_TARGET || 'x86_64-native-linuxapp-gcc';
 //    var cmd = 'make -j EXTRA_CFLAGS=\"-g -O3\"';
-    var cmd = 'make -j';
+    var cmd = 'env RTE_SDK=' + sdk + ' RTE_TARGET=' + target + ' make -j';
     console.log('Build using:' + cmd);
     cp.execSync(cmd, {
         stdio: 'inherit'
     });
 });
-gulp.task('debug', function () {
+gulp.task('debug-build', function () {
     var env = process.env.RTE_TARGET || 'x86_64-native-linuxapp-gcc';
     var cmd = 'make -j EXTRA_CFLAGS=\"-g -O0\"';
     console.log('Build using:' + cmd);

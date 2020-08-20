1b851a0e52ef8ec12dc1f68e24d2ebc5526731e0
Bruce Richardson <bruce.richardson@intel.com>
2020-08-20 20:52:14 +0100

    build: simplify versioning
    Rather than splitting up the version string to have it put back together
    later, we can just pass the whole string from the build to the app code.
    
    Signed-off-by: Bruce Richardson <bruce.richardson@intel.com>

--- app/pktgen-version.h
+++ app/pktgen-version.h
@@ -13,43 +13,10 @@
 extern "C" {
 #endif
 
-#define PKTGEN_VER_PREFIX	"Pktgen"
+#define PKTGEN_VER_PREFIX	"Pktgen "
 #define PKTGEN_VER_CREATED_BY	"Keith Wiles"
 
-#define STRINGIFY(x)	#x
-#define TOSTRING(x)	STRINGIFY(x)
-
-#define PKTGEN_VER_YEAR		__YEAR
-#define PKTGEN_VER_MONTH	__MONTH
-#define PKTGEN_VER_MINOR	__MINOR
-#define PKTGEN_VER_SUFFIX	__SUFFIX
-#define PKTGEN_VER_RELEASE	__RELEASE
-
-#define PKTGEN_VERSION		pktgen_version_str()
-
-static inline const char *
-pktgen_version_str(void)
-{
-	static char version[64];
-
-	if (version[0] != 0)
-		return version;
-	if (strlen(PKTGEN_VER_SUFFIX) == 0)
-		snprintf(version, sizeof(version), "%s %d.%02d.%d",
-				PKTGEN_VER_PREFIX,
-				PKTGEN_VER_YEAR,
-				PKTGEN_VER_MONTH,
-				PKTGEN_VER_MINOR);
-	else
-		snprintf(version, sizeof(version), "%s %d.%02d.%d%s%d",
-				PKTGEN_VER_PREFIX,
-				PKTGEN_VER_YEAR,
-				PKTGEN_VER_MONTH,
-				PKTGEN_VER_MINOR,
-				(strlen(PKTGEN_VER_SUFFIX) != 0)? "-rc" : TOSTRING(PKTGEN_VER_SUFFIX),
-				PKTGEN_VER_RELEASE);
-	return version;
-}
+#define PKTGEN_VERSION	PKTGEN_VER_PREFIX __PROJECT_VERSION
 
 #ifdef __cplusplus
 }

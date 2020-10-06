0a237a4577d6c4ad216965a3d90d46b1b89c3f74
Keith Wiles <keith.wiles@intel.com>
2020-10-06 12:32:14 -0400

    fix ENA memory configuration
    Signed-off-by: Keith Wiles <keith.wiles@intel.com>

--- app/pktgen-constants.h
+++ app/pktgen-constants.h
@@ -15,11 +15,12 @@ extern "C" {
 #endif
 
 enum {
+#ifndef RTE_LIBRTE_VMXNET3_PMD
 	DEFAULT_PKT_BURST       = 64,	/* Increasing this number consumes memory very fast */
-#ifdef RTE_LIBRTE_VMXNET3_PMD
 	DEFAULT_RX_DESC         = (DEFAULT_PKT_BURST * 8 * 2),
 	DEFAULT_TX_DESC         = DEFAULT_RX_DESC * 2,
 #else
+	DEFAULT_PKT_BURST       = 32,	/* Increasing this number consumes memory very fast */
 	DEFAULT_RX_DESC         = (DEFAULT_PKT_BURST * 8),
 	DEFAULT_TX_DESC         = DEFAULT_RX_DESC * 2,
 #endif

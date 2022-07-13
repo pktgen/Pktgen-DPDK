13925935d7e8eefb62d76e6598eaf11e2532ae65
Keith Wiles <keith.wiles@intel.com>
2022-07-13 13:47:12 -0500

    Update rfc2544 script and increase number of mbufs
    Signed-off-by: Keith Wiles <keith.wiles@intel.com>

--- app/pktgen-constants.h
+++ app/pktgen-constants.h
@@ -15,17 +15,11 @@ extern "C" {
 #endif
 
 enum {
-#if !defined(RTE_LIBRTE_VMXNET3_PMD) && !defined(RTE_NET_VMXNET3)
     DEFAULT_PKT_BURST = 64, /* Increasing this number consumes memory very fast */
-    DEFAULT_RX_DESC   = (DEFAULT_PKT_BURST * 8 * 2),
-    DEFAULT_TX_DESC   = DEFAULT_RX_DESC * 2,
-#else
-    DEFAULT_PKT_BURST = 128, /* Increasing this number consumes memory very fast */
-    DEFAULT_RX_DESC   = (DEFAULT_PKT_BURST * 8),
-    DEFAULT_TX_DESC   = DEFAULT_RX_DESC * 2,
-#endif
+    DEFAULT_RX_DESC   = (DEFAULT_PKT_BURST * 16),
+    DEFAULT_TX_DESC   = (DEFAULT_RX_DESC * 2),
 
-    MAX_MBUFS_PER_PORT = (DEFAULT_TX_DESC * 8), /* number of buffers to support per port */
+    MAX_MBUFS_PER_PORT = (DEFAULT_TX_DESC * 10), /* number of buffers to support per port */
     MAX_SPECIAL_MBUFS  = 64,
     MBUF_CACHE_SIZE    = (MAX_MBUFS_PER_PORT / 8),
 

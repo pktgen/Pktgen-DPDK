73ddea1daa62c2de27e3d55501892a3c2b237de3
Keith Wiles <pktgen@users.noreply.github.com>
2023-03-10 09:56:58 -0600

    Latency update (#168)
    * fix latency counter and values
    
    Clean up the latency counter and values to be easier to read. The old
    latency values in micro-seconds was not always correct. The code now
    injects timestamp packets into the output stream, which allows latency
    measurements to be calculated on any mode.
    
    new format of portstats for latency:
      [0] = {
        ["unknown_pkts"] = 0,
        ["dropped_pkts"] = 0,
        ["mac_addr"] = "3C:FD:FE:E4:38:40",
        ["latency"] = {
          ["rate_ms"] = 10,
          ["jitter_count"] = 0,
          ["jitter_threshold"] = 50,
          ["max_cycles"] = 0,
          ["num_skipped"] = 0,
          ["max_us"] = 0.0,
          ["min_us"] = 0.0,
          ["num_pkts"] = 0,
          ["avg_cycles"] = 0,
          ["avg_us"] = 0.0,
          ["min_cycles"] = 0,
        },
        ["arp_pkts"] = 0,
        ["tx_failed"] = 0,
        ["echo_pkts"] = 0,
        ["ipv6_pkts"] = 0,
        ["ip_pkts"] = 0,
        ["vlan_pkts"] = 0,
      },
    
    Signed-off-by: Keith Wiles <keith.wiles@intel.com>
    
    * fix merge issue
    
    Signed-off-by: Keith Wiles <keith.wiles@intel.com>
    
    * update change log for latancy updates
    
    Signed-off-by: Keith Wiles <keith.wiles@intel.com>
    
    * add latency example script
    
    Signed-off-by: Keith Wiles <keith.wiles@intel.com>
    
    * allow a string or number for a portlist
    
    Signed-off-by: Keith Wiles <keith.wiles@intel.com>
    
    ---------
    
    Signed-off-by: Keith Wiles <keith.wiles@intel.com>
    Co-authored-by: Keith Wiles <keith.wiles@intel.com>

---
+++ scripts/latency.lua
@@ -0,0 +1,33 @@
+-- Latency Demo script
+--
+-- SPDX-License-Identifier: BSD-3-Clause
+
+package.path = package.path ..";?.lua;test/?.lua;app/?.lua;../?.lua"
+
+require "Pktgen";
+
+local port = 2;
+local sleeptime = 10
+
+pktgen.stop(port);
+
+printf("Setup port %d for latency packets\n", port);
+pktgen.clr();
+pktgen.delay(100);
+
+pktgen.latency(port, "enable");
+
+pktgen.start(port);
+printf("Sleep for %d seconds\n", sleeptime);
+pktgen.sleep(sleeptime);
+printf("Sleep is done\n");
+
+pktgen.stop(port);
+pktgen.latency(port, "disable");
+
+printf("Sendport Type: %s\n", type(port));
+prints("SendPort", pktgen.pktStats(port));
+local port_stats = pktgen.pktStats(port);
+
+printf("Number of latency packets on port %d : %d\n",
+    port, port_stats[port].latency.num_pkts);

bfa28413e34fce4e4ceedce5df136852c0e223c6
Keith Wiles <keith.wiles@intel.com>
2024-05-04 11:27:44 -0500

    update pre-commit to handle existing files
    Signed-off-by: Keith Wiles <keith.wiles@intel.com>

--- format-clang.sh
+++ format-clang.sh
@@ -1,8 +1,10 @@
 #! /bin/bash
 #
 
-for filename in $(find . -path ./builddir -prune -o -name "*.[ch]"); do
-    echo "Process File: $filename"
-    clang-format -style=file -i $filename; # git add $filename;
+for filename in $(find . -path ./Builddir -prune -o -name "*.[ch]"); do
+    if [ -f $filename ]; then
+        echo "Process File: $filename"
+        clang-format -style=file -i $filename; # git add $filename;
+    fi
 done
 
